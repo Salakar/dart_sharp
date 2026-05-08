5760e48b241dd516bbee0f3e358dc5047457e0e7
Salakar <mike.diarmid@gmail.com>
2026-05-08 14:04:52 +0100

    feat: encode lossy WebP diagonal chroma AC

--- packages/dsharp/lib/src/codecs/webp_vp8_encoder_chroma.dart
+++ packages/dsharp/lib/src/codecs/webp_vp8_encoder_chroma.dart
@@ -29,6 +29,7 @@ List<List<_Vp8ChromaAc>> _lossyChromaAcBlocks(
     build(_lossyChromaHorizontalAcBlocks),
     build(_lossyChromaVerticalAcBlocks),
     build(_lossyChromaSecondVerticalAcBlocks),
+    build(_lossyChromaDiagonalAcBlocks),
   ];
 }
 
@@ -104,6 +105,35 @@ List<_Vp8ChromaAc> _lossyChromaSecondVerticalAcBlocks(
       for (var block = 0; block < 4; block += 1) {
         blocks.add(
           _lossyChromaSecondVerticalAcBlock(
+            rgba,
+            width: width,
+            height: height,
+            quality: quality,
+            mbX: mbX,
+            mbY: mbY,
+            block: block,
+          ),
+        );
+      }
+    }
+  }
+  return blocks;
+}
+
+List<_Vp8ChromaAc> _lossyChromaDiagonalAcBlocks(
+  Uint8List rgba, {
+  required int width,
+  required int height,
+  required int quality,
+}) {
+  final mbCols = (width + 15) >> 4;
+  final mbRows = (height + 15) >> 4;
+  final blocks = <_Vp8ChromaAc>[];
+  for (var mbY = 0; mbY < mbRows; mbY += 1) {
+    for (var mbX = 0; mbX < mbCols; mbX += 1) {
+      for (var block = 0; block < 4; block += 1) {
+        blocks.add(
+          _lossyChromaDiagonalAcBlock(
             rgba,
             width: width,
             height: height,
@@ -299,5 +329,71 @@ _Vp8ChromaAc _lossyChromaSecondVerticalAcBlock(
   return _Vp8ChromaAc(
     u: _clampDctCoefficient(((outerUAverage - innerUAverage) * 3) ~/ 4),
     v: _clampDctCoefficient(((outerVAverage - innerVAverage) * 3) ~/ 4),
+  );
+}
+
+_Vp8ChromaAc _lossyChromaDiagonalAcBlock(
+  Uint8List rgba, {
+  required int width,
+  required int height,
+  required int quality,
+  required int mbX,
+  required int mbY,
+  required int block,
+}) {
+  final blockX = block & 1;
+  final blockY = block >> 1;
+  final xStart = mbX * 16 + blockX * 8;
+  final yStart = mbY * 16 + blockY * 8;
+  if (xStart >= width || yStart >= height) {
+    return const _Vp8ChromaAc(u: 0, v: 0);
+  }
+  var diagonalU = 0;
+  var diagonalV = 0;
+  var antiDiagonalU = 0;
+  var antiDiagonalV = 0;
+  var diagonalPixels = 0;
+  var antiDiagonalPixels = 0;
+  final xEnd = xStart + 8 < width ? xStart + 8 : width;
+  final yEnd = yStart + 8 < height ? yStart + 8 : height;
+  for (var y = yStart; y < yEnd; y += 1) {
+    var offset = (y * width + xStart) * 4;
+    for (var x = xStart; x < xEnd; x += 1) {
+      final color = _rgbToVp8Yuv(
+        _quantizeLossyColor(
+          _Rgb(rgba[offset], rgba[offset + 1], rgba[offset + 2]),
+          quality,
+        ),
+      );
+      final isLeft = x - xStart < 4;
+      final isTop = y - yStart < 4;
+      if (isLeft == isTop) {
+        diagonalU += color.u;
+        diagonalV += color.v;
+        diagonalPixels += 1;
+      } else {
+        antiDiagonalU += color.u;
+        antiDiagonalV += color.v;
+        antiDiagonalPixels += 1;
+      }
+      offset += 4;
+    }
+  }
+  if (diagonalPixels == 0 || antiDiagonalPixels == 0) {
+    return const _Vp8ChromaAc(u: 0, v: 0);
+  }
+  final diagonalUAverage = (diagonalU + diagonalPixels ~/ 2) ~/ diagonalPixels;
+  final diagonalVAverage = (diagonalV + diagonalPixels ~/ 2) ~/ diagonalPixels;
+  final antiDiagonalUAverage =
+      (antiDiagonalU + antiDiagonalPixels ~/ 2) ~/ antiDiagonalPixels;
+  final antiDiagonalVAverage =
+      (antiDiagonalV + antiDiagonalPixels ~/ 2) ~/ antiDiagonalPixels;
+  return _Vp8ChromaAc(
+    u: _clampDctCoefficient(
+      ((diagonalUAverage - antiDiagonalUAverage) * 3) ~/ 4,
+    ),
+    v: _clampDctCoefficient(
+      ((diagonalVAverage - antiDiagonalVAverage) * 3) ~/ 4,
+    ),
   );
 }
