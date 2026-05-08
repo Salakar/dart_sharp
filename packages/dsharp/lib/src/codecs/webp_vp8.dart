c8cd314fcf9d5dcbcc7a8fbb5c408357d0614179
Salakar <mike.diarmid@gmail.com>
2026-05-08 14:37:01 +0100

    refactor: split lossy WebP mixed chroma AC helpers

--- packages/dsharp/lib/src/codecs/webp_vp8.dart
+++ packages/dsharp/lib/src/codecs/webp_vp8.dart
@@ -10,6 +10,7 @@ import 'webp_vp8_bool.dart';
 
 part 'webp_vp8_encoder.dart';
 part 'webp_vp8_encoder_chroma.dart';
+part 'webp_vp8_encoder_chroma_mixed.dart';
 part 'webp_vp8_encoder_luma.dart';
 part 'webp_vp8_encoder_luma_mixed.dart';
 part 'webp_vp8_encoder_pixels.dart';
