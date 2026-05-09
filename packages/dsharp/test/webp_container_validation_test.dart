94a1544c558f929aba89629833be9f160a2a42de
Salakar <mike.diarmid@gmail.com>
2026-05-09 05:02:16 +0100

    test: cover invalid webp vp8x lengths

--- packages/dsharp/test/webp_container_validation_test.dart
+++ packages/dsharp/test/webp_container_validation_test.dart
@@ -356,6 +356,20 @@ void main() {
     }
   });
 
+  test('rejects VP8X chunks with invalid length', () async {
+    final bytes = extendedSolidVp8Webp(width: 1, height: 1);
+    ByteData.sublistView(bytes).setUint32(16, 9, Endian.little);
+
+    await expectLater(
+      ImagePipeline.fromBytes(bytes).metadata(),
+      throwsA(isA<InvalidImageException>()),
+    );
+    await expectLater(
+      ImagePipeline.fromBytes(bytes).toPixelImage(),
+      throwsA(isA<InvalidImageException>()),
+    );
+  });
+
   test('rejects VP8X chunks with reserved bytes', () async {
     for (final offset in <int>[21, 22, 23]) {
       final bytes = extendedSolidVp8Webp(width: 1, height: 1);
