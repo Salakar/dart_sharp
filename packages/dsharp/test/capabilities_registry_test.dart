5310500a878cd0af9b37d1f55c664ffe09de5b39
Salakar <mike.diarmid@gmail.com>
2026-05-11 01:08:48 +0100

    test: cover unsupported codec registry failures

--- packages/dsharp/test/capabilities_registry_test.dart
+++ packages/dsharp/test/capabilities_registry_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:dsharp/dsharp.dart';
 import 'package:dsharp/src/codecs/unsupported_codec.dart';
 import 'package:test/test.dart';
@@ -29,6 +31,48 @@ void main() {
       if (codec is UnsupportedImageCodec) {
         expect(codec.reason, support.reason, reason: format.id);
       }
+    }
+  });
+
+  test('unsupported registry codecs reject encode and decode consistently', () {
+    final registry = CodecRegistry.defaultRegistry();
+    final image = PixelImage.fromRawPixels(
+      RawPixels(
+        bytes: Uint8List.fromList(<int>[0, 0, 0, 255]),
+        width: 1,
+        height: 1,
+        channels: ChannelCount.four,
+      ),
+    );
+    final formats = ImageFormat.values.where(
+      (format) =>
+          format != ImageFormat.unknown &&
+          registry.codecFor(format) is UnsupportedImageCodec,
+    );
+
+    for (final format in formats) {
+      expect(
+        () => registry.decode(Uint8List(0), format: format),
+        throwsA(
+          isA<UnsupportedCodecException>().having(
+            (error) => error.message,
+            'message',
+            contains('${format.id} decode is unsupported'),
+          ),
+        ),
+        reason: format.id,
+      );
+      expect(
+        () => registry.encode(image, format: format),
+        throwsA(
+          isA<UnsupportedCodecException>().having(
+            (error) => error.message,
+            'message',
+            contains('${format.id} encode is unsupported'),
+          ),
+        ),
+        reason: format.id,
+      );
     }
   });
 }
